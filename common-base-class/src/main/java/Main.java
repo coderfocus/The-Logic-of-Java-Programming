9564b48160ce013c5c04f8b11c4a08af5452bf69
coderfocus <songwenjie830@gmail.com>
2019-07-10 11:05:38 +0800

    feat(generic): 新增 Generic泛型类

--- common-base-class/src/main/java/Main.java
+++ common-base-class/src/main/java/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args){
         PackingClass.forExample();
 
+        Pair<String> stringPair = new Pair<>("name","coderfocus");
+        String key = stringPair.getFirst();
+        String val  = stringPair.getSecond();
 
+        Pair<Integer> integerPair = new Pair<>(1,100);
+        int min = integerPair.getFirst();
+        Integer max = integerPair.getSecond();
     }
 }
