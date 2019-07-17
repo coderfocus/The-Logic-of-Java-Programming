8dfe13399052bceed89d748004ec967d956cccdc
coderfocus <songwenjie830@gmail.com>
2019-07-17 15:36:09 +0800

    feat(Java): 新增 为什么Calendar 类中不使用枚举而使用静态整型常？？？

--- enum/src/main/java/SizeReplace.java
+++ enum/src/main/java/SizeReplace.java
@@ -1,4 +1,7 @@
-//在枚举之前，一般是在类中声明静态整型变量实现类似功能
+//在枚举之前，一般是在类中声明静态整型常量实现类似功能
+//考虑一下 Calendar 类中为什么不使用枚举而使用静态整型常？？？
+// Calendar since JDK 1.1
+// enum since JDK 1.5
 public class SizeReplace {
     public static final int SMALL = 0;
     public static final int MEDIUM = 1;
